3c2030d97d02cc3ffd7a67fdab5ec8fcca3538d7
Goldenlion5648 <coboudinot@gmail.com>
2021-09-04 18:53:22 -0400

    days 8 and 9 done

---
+++ day8.py
@@ -0,0 +1,56 @@
+from itertools import *
+from collections import *
+from math import *
+import re
+from AoCLibrary import *
+with open("input8.txt") as f:
+    # a = list(map(int,f.read().strip().split("\n")))
+    a = f.read().strip().split("\n")
+board = defaultdict(bool)
+wide = 50
+tall = 6
+#part 1 done in 30 minutes
+#part 2 done in 30:34 minutes
+
+# def show_board(board):
+#     w = max(x[1] for x in board) + 1
+#     h = max(x[0] for x in board) + 1
+#     for j in range(h):
+#         for i in range(w):
+#             if (board[(j, i)]):
+#                 print("#", end='')
+#             else:
+#                 print(".", end='')
+#         print()
+
+for line in a:
+    if line.startswith("rect"):
+        _, b = line.split(" ")
+        l, w = map(int, b.split("x"))
+        print(l, w)
+        for i in range(l):
+            for j in range(w):
+                board[(j, i)] = True
+    elif "row" in line:
+        cur = line.split(" ")
+        y = int(cur[2][2:])
+        amount = int(cur[-1])
+        print("amount", amount)
+        temp = board[(0, y)]
+        temp_board = board.copy()
+        for i in range(wide):
+            board[(y, (i+amount) % wide)] = temp_board[(y, i)]
+    elif "column" in line:
+        cur = line.split(" ")
+        x = int(cur[2][2:])
+        amount = int(cur[-1])
+        print("amount", amount)
+        temp = board[(0, x)]
+        temp_board = board.copy()
+        for i in range(tall):
+            board[((i+amount) % tall, x)] = temp_board[(i, x)]
+        # board[((i - temp) % tall, x)] = temp
+print(board)
+show_board(board)
+ans(sum(board.values()))
+
---
+++ day9.py
@@ -0,0 +1,69 @@
+from itertools import *
+from collections import *
+from math import *
+import re
+from AoCLibrary import *
+with open("input9.txt") as f:
+    # a = list(map(int,f.read().strip().split("\n")))
+    # a = f.read().strip().split("\n")
+    a = f.read().strip()
+#part 1 in 15:44
+
+seen = dict()
+
+def expand(a):
+    left = 0
+    total = 0
+    while True:
+        if all(j not in a for j in "()"):
+            seen[a] = len(a)
+            total += len(a)
+            return total
+        try:
+            old = left
+            left = a.index("(", left)
+            total += left - old
+            right = a.index(")", left + 1)
+        except:
+            return total
+        amount, rep = map(int, a[left+1:right].split("x"))
+        end = right + amount + 1
+        capture = a[right + 1:end]
+        if capture not in seen:
+            seen[capture] = expand(capture)
+        total += seen[capture] * rep
+        # print(capture, "*", rep, total)
+        # extra = capture * rep
+        a = a[end:]
+        # print(a)
+
+def get_value(capture):
+    if capture in seen:
+        return seen[capture]
+    seen[capture] = expand(capture)
+    return seen[capture]
+    
+
+def part1(a):
+    cur = 0
+    while True:
+        try:
+            cur = a.index("(", cur)
+            other = a.index(")", cur + 1)
+        except:
+            break
+        x, rep = map(int, a[cur+1:other].split("x"))
+        end = other+x + 1
+        capture = a[other + 1:end]
+        extra = capture * rep
+        a = a[:cur] + extra + a[end:]
+        cur += len(extra)
+        # print(a)
+
+    ans(len(a.strip()), should_exit=False)
+
+
+# print(seen)
+part1(a)
+seen[a] = get_value(a)
+ans(seen[a])
