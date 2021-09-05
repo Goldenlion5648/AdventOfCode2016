d548e40d0fccd16e02bcb31a415446ee4013ec50
Goldenlion5648 <coboudinot@gmail.com>
2021-09-05 14:26:32 -0400

    day 13 done

---
+++ day13.py
@@ -0,0 +1,26 @@
+from AoCLibrary import *
+
+with open("input13.txt") as f:
+    # a = list(map(int,f.read().strip().split("\n")))
+    # a = f.read().strip().split("\n")
+    num = int(f.read().strip())
+# part 1 done in about 16 minutes
+# part 2 done in about 20 minutes (off by 1 error, then doubt)
+# num = 10
+xDim = 60
+yDim = 60
+goalX, goalY = 31, 39
+# goalX, goalY = 7, 4
+board = defaultdict(str)
+for y in range(yDim):
+    for x in range(xDim):
+        val = x*x + 3*x + 2*x*y + y + y*y
+        val += num
+        b = bin(val)[2:]
+        c = b.count('1')
+        board[(y, x)] = c % 2 == 1
+
+# show_board(board)
+
+ans(bfs(1, 1, goalY, goalX, board)[0], should_exit=False)
+ans(len(bfs(1, 1, goalY, goalX, board, 50)[1]))
