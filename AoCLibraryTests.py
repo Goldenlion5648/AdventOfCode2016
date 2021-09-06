cf60ec4496ca7c569a244342e60f50db81317d39
Goldenlion5648 <coboudinot@gmail.com>
2021-09-06 09:38:06 -0400

    add library tests

---
+++ AoCLibraryTests.py
@@ -0,0 +1,19 @@
+from AoCLibrary import *
+
+WORDS = \
+'''1 cat and 34 dogs
+19 cat and -34 dogs
+43543 cat and 3 dogs
+'''
+
+NUMBERS= \
+    '''344
+    4
+    5
+    
+    7456765'''
+
+
+
+print(nums2(WORDS))
+print(nums(NUMBERS))
